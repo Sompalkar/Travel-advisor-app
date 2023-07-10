c2cf0622407366416b2c6ad6e40c52029683529a
Sompalkar <palkarsom1@gmail.com>
2023-07-10 11:20:56 +0530

    first commit

---
+++ src/Components/index.js
@@ -0,0 +1,7 @@
+import Sidebar from "./Sidebar";
+
+import List from "./List";
+import Map from "./Map";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+export { Sidebar, Map, List, SignIn, SignUp };
